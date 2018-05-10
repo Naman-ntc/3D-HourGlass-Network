19bd738ea5478f5d8c07b03b84bef2129c642542
Naman Jain <naman1205jain@gmail.com>
2018-05-10 16:52:25 +0530

    testScript better

--- testScript.py
+++ testScript.py
@@ -1,15 +1,21 @@
 import os
+import sys
 import torch
 import numpy as np
 import cv2
 from HourGlassNet3D import *
 import time
 
-all_frames = os.listdir('../train_frames/train927')
+argumentList = sys.argv[1:]
+
+assert argumentList[0] == "-imageFolder", "Give an Image Folder with -imageFolder Flag"
+
+all_frames = os.listdir(argumentList[1])
+
 n_frames = len(all_frames)
 frames_seq = np.zeros((1, 3, n_frames, 256, 256))
 for idx, frame in enumerate(all_frames):
-	frames_seq[0,:,idx,:,:] = cv2.imread('../train_frames/train927/'+ frame).transpose(2,0,1)
+	frames_seq[0,:,idx,:,:] = cv2.imread(argumentList[1] + frame).transpose(2,0,1)
 
 frames_seq = torch.from_numpy(frames_seq).float() /256
 frames_var = torch.autograd.Variable(frames_seq).float().cuda()

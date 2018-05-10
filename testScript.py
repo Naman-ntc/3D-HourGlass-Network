34b6983ffa5d190ccd1e36fba27f07f43a957a69
Naman <naman1205jain@gmail.com>
2018-05-10 09:28:27 +0000

    testing worked well

--- testScript.py
+++ testScript.py
@@ -4,18 +4,19 @@ import numpy as np
 import cv2
 from HourGlassNet3D import *
 
-all_frames = os.listdir('../train_frames/train2')
+all_frames = os.listdir('../train_frames/train927')
 n_frames = len(all_frames)
 frames_seq = np.zeros((1, 3, n_frames, 256, 256))
 for idx, frame in enumerate(all_frames):
-	frames_seq[0,:,idx,:,:] = cv2.imread('../train_frames/train2/'+ frame).transpose(2,0,1)
+	frames_seq[0,:,idx,:,:] = cv2.imread('../train_frames/train927/'+ frame).transpose(2,0,1)
 
 frames_seq = torch.from_numpy(frames_seq).float() /256
 frames_var = torch.autograd.Variable(frames_seq).float().cuda()
 
-hg = HourglassNet3D(32)
+hg = HourglassNet3D(64,2,1,4)
 hg = hg.cuda()
-hg(frames_var)
+while(True):
+	print(hg(frames_var)[-1].shape)
 
 
 """

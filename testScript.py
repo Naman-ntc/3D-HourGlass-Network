c85f507f11e7c579233a7afadaafc69db61e4d3a
Naman Jain <naman1205jain@gmail.com>
2018-05-10 14:02:05 +0530

    corr\ction

--- testScript.py
+++ testScript.py
@@ -8,7 +8,7 @@ all_frames = os.listdir('../train_frames/train2')
 n_frames = len(all_frames)
 frames_seq = np.zeros((1, 3, n_frames, 256, 256))
 for idx, frame in enumerate(all_frames):
-	frames_seq[0,0:2,idx,:,:] = cv2.imread('../train_frames/train2/'+ frame).transpose(2,0,1)
+	frames_seq[0,:,idx,:,:] = cv2.imread('../train_frames/train2/'+ frame).transpose(2,0,1)
 
 frames_seq = torch.from_numpy(frames_seq).float() /256
 frames_var = torch.autograd.Variable(frames_seq).float().cuda()

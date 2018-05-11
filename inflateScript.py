205c3c0e3bc35fcf80e92ca90294ec415666f5c1
Naman Jain <naman1205jain@gmail.com>
2018-05-11 15:28:40 +0530

    saved infalted model

--- inflateScript.py
+++ inflateScript.py
@@ -12,6 +12,9 @@ model3d = torch.load('models/model3d.pth')
 
 pickle.Unpickler = partial(pickle.Unpickler, encoding="latin1")
 pickle.load = partial(pickle.load, encoding="latin1")
-model = torch.load('models/hgreg-3d.pth', map_location=lambda storage, loc: storage)
+model = torch.load('models/hgreg-3d.pth') #, map_location=lambda storage, loc: storage)
 
 inflateHourglassNet(model3d, model)
+
+torch.save(model3d,open('inflatedModel.pth','wb'))
+

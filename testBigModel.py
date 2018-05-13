f693ba732b6d6e5a811bcd262523dd2252d45ca9
Naman Jain <naman1205jain@gmail.com>
2018-05-13 18:27:41 +0530

    test script for bigmodel

---
+++ testBigModel.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import numpy as np
+import cv2
+import torch
+from HourGlassNet3D import *
+from Loss import *
+from SoftArgmax import *
+
+argumentList = sys.argv[1:]
+
+assert argumentList[0] == "-imageFolder", "Give an Image Folder with -imageFolder Flag"
+
+
+argumentList[1] += "/"
+all_frames = os.listdir(argumentList[1])
+
+n_frames = len(all_frames)
+frames_seq = np.zeros((1, 3, n_frames, 256, 256))
+for idx, frame in enumerate(all_frames):
+	frames_seq[0,:,idx,:,:] = cv2.imread(argumentList[1] + frame).transpose(2,0,1)
+
+frames_seq = torch.from_numpy(frames_seq[:,:,1:25,:,:]).float() /256
+frames_var = torch.autograd.Variable(frames_seq).float().cuda()
+
+hg = HourglassNet3D(256,1,2,4, 16)
+hg = hg.cuda()
+
+heatmaps = hg(frames_var)
+
+heatmapsLen = len(heatmaps)
+
+loss = 0
+
+for i in range(heatmapsLen):
+	temp = SoftArgMax(heatmaps[i])
+	temp1 += (torch.randn(temp.size()) + 1)*256
+	loss  += JointSquaredError(temp, temp1)
+
+loss.backward()

2a907e4915698ab6a2312ae4ab954c4da01fca8f
Naman Jain <naman1205jain@gmail.com>
2018-05-19 18:57:12 +0530

    debugin losses

--- src/Losses.py
+++ src/Losses.py
@@ -11,8 +11,10 @@ def Joints2DHeatMapsSquaredError(input, target):
 	Takes input as (N,C,D,H,W) and similar target (Here C is number of channels equivalent to number of joints
 	and H,W are equal to the input image dimensions (i.e. 256 each))
 	"""
-	#assert len(input.shape) == 5, "Joints2DHeatMapsSquaredError"
 	N = input.size()[0]
+	input = input.cuda()
+	print(input.shape)
+	print(target.shape)
 	return lossfunc(input.view(N,-1), target.view(N,-1))
 
 def Joints2DArgMaxSquaredError(input, target):
@@ -20,8 +22,10 @@ def Joints2DArgMaxSquaredError(input, target):
 	"""
 	Takes input as (N,C,D,2) and similar target (Here C is number of channels equivalent to number of joints)
 	"""
-	#assert len(input.shape) == 4 & input.shape[3] == 2, "Joints2DArgMaxSquaredError"
 	N = input.size()[0]
+	input = input.cuda()
+	print(input.shape)
+	print(target.shape)	
 	return lossfunc(input.view(N,-1), target.view(N,-1))
 
 def JointsDepthSquaredError(input, target):
@@ -29,7 +33,8 @@ def JointsDepthSquaredError(input, target):
 	"""
 	Takes input as (N,C,D,1) and similar target (Here C is number of channels equivalent to number of joints)
 	"""	
-	#assert len(input.shape) == 4 & input.size()[3] == 1, "JointsDepthSquaredError"
 	N = input.size()[0]
 	input = input.cuda()
+	print(input.shape)
+	print(target.shape)
 	return lossfunc(input.view(N,-1), target.view(N,-1))

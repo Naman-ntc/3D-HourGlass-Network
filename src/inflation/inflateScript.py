3545e39dea4d1fc88f8af969617a58f66f2e194e
Naman Jain <naman1205jain@gmail.com>
2018-05-20 17:47:18 +0530

    2d model and inflation script

--- src/inflation/inflateScript.py
+++ src/inflation/inflateScript.py
@@ -1,21 +1,25 @@
+import sys
+sys.path.insert(0,'..')
 import pickle
 from functools import partial
-from models.hg_3d import *
-from HourGlassNet3D import *
-from HourGlass3D import *
-from Layers3D import *
-from Inflate import *
+from twod.hg_3d import *
+from model.Pose3D import *
+from model.DepthRegressor3D import *
+from model.HourGlassNet3D import *
+from model.HourGlass3D import *
+from model.Layers3D import *
+from inflation.Inflate import *
 import torch
 
 
-model3d = HourglassNet3D(256,2,2,4,16)
-torch.save(model3d, open('models/model3d.pth', 'wb'))
+model3d = Pose3D()
+
 
 pickle.Unpickler = partial(pickle.Unpickler, encoding="latin1")
 pickle.load = partial(pickle.load, encoding="latin1")
 model = torch.load('models/hgreg-3d.pth') #, map_location=lambda storage, loc: storage)
 
-inflateHourglassNet(model3d, model)
+inflatePose3D(model3d, model)
 
 torch.save(model3d,open('inflatedModel.pth','wb'))
 

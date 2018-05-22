20b868240a8909420116cddec65809869b1b729a
Naman Jain <naman1205jain@gmail.com>
2018-05-22 17:39:57 +0530

    Auto stash before merge of "master" and "origin/master"

--- src/main.py
+++ src/main.py
@@ -62,13 +62,13 @@ def main():
 		#logger.scalar_summary('acc_train', acc_train, epoch)
 		#logger.scalar_summary('mpjpe_train', mpjpe_train, epoch)
 		logger.scalar_summary('loss3d_train', loss3d_train, epoch)
-		# if epoch % opt.valIntervals == 0:
-		# 	loss_val, acc_val, mpjpe_val, loss3d_val = val(epoch, opt, val_loader, model, criterion)
+		if epoch % opt.valIntervals == 0:
+			loss_val, acc_val, mpjpe_val, loss3d_val = val(epoch, opt, val_loader, model, criterion)
 		# 	logger.scalar_summary('loss_val', loss_val, epoch)
 		# 	logger.scalar_summary('acc_val', acc_val, epoch)
 		# 	logger.scalar_summary('mpjpe_val', mpjpe_val, epoch)
 		# 	logger.scalar_summary('loss3d_val', loss3d_val, epoch)
-		torch.save(model, os.path.join(opt.saveDir, 'model_{}.pth'.format(epoch)))
+			torch.save(model, os.path.join(opt.saveDir, 'model_{}.pth'.format(epoch)))
 		# 	logger.write('{:8f} {:8f} {:8f} {:8f} {:8f} {:8f} {:8f} {:8f} \n'.format(loss_train, acc_train, mpjpe_train, loss3d_train, loss_val, acc_val, mpjpe_val, loss3d_val))
 		# else:
 		# 	logger.write('{:8f} {:8f} {:8f} {:8f} \n'.format(loss_train, acc_train, mpjpe_train, loss3d_train))

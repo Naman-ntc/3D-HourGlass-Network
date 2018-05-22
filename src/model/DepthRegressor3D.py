c07fb4053839617668d1d8f0ede6aa647b46b093
Naman <naman1205jain@gmail.com>
2018-05-22 11:57:06 +0530

    hmm

--- src/model/DepthRegressor3D.py
+++ src/model/DepthRegressor3D.py
@@ -17,7 +17,7 @@ class DepthRegressor3D(nn.Module):
 			reg_.append(nn.MaxPool3d((1,2,2), (1,2,2)))
 
 		self.reg = nn.Sequential(* reg_)
-		
+
 		self.fc = nn.Linear(self.nChannels*self.nRegFrames*4*4, 16*self.nRegFrames)
 
 	def forward(self, input):
@@ -27,9 +27,9 @@ class DepthRegressor3D(nn.Module):
 		slides = D/ self.nRegFrames
 		z = torch.zeros(N, 16, D, 1)
 		for i in range(int(slides)):
-			z[:,:,16*i:16*i+16,:] = self.fc(out[:,:,16*i:16*i+16,:,:].reshape(-1, 16*self.nRegFrames*self.nChannels)).reshape(N,16,D,1)
+			z[:,:,16*i:16*i+16,:] = self.fc(out[:,:,16*i:16*i+16,:,:].reshape(-1, 16*self.nRegFrames*self.nChannels)).reshape(N,16,self.nRegFrames,1)
 		rem = D % self.nRegFrames
 
 		if (rem != 0):
-			z[:,:,16*int(slides):D,:] = self.fc(out[:,:,D-16:D,:,:].reshape(-1, 16*self.nRegFrames*self.nChannels)).reshape(N,16,D,1)[:,:,16 + 16*int(slides) - D:16,:]
+			z[:,:,16*int(slides):D,:] = self.fc(out[:,:,D-16:D,:,:].reshape(-1, 16*self.nRegFrames*self.nChannels)).reshape(N,16,self.nRegFrames,1)[:,:,16 + 16*int(slides) - D:16,:]
 		return z

7f984ac784ea67b980532f485c4ee1518b3b26ef
Naman Jain <naman1205jain@gmail.com>
2018-05-25 15:10:26 +0530

    multi batch working

--- src/model/DepthRegressor3D.py
+++ src/model/DepthRegressor3D.py
@@ -27,7 +27,7 @@ class DepthRegressor3D(nn.Module):
 		slides = D/ self.nRegFrames
 		z = torch.zeros(N, self.nJoints, D, 1)
 		for i in range(int(slides)):
-			temp1 = out[:,:,self.nRegFrames*i:self.nRegFrames*(i+1),:,:].transpose(1,2).view(N, -1)
+			temp1 = out[:,:,self.nRegFrames*i:self.nRegFrames*(i+1),:,:].transpose(1,2).contiguous().view(N, -1)
 			temp2 = self.fc(temp1).view(N,self.nRegFrames, self.nJoints).transpose(1,2).unsqueeze(-1)
 			z[:,:,self.nRegFrames*i:self.nRegFrames*(i+1),:] = temp2
 		rem = D % self.nRegFrames

d71ec80fce84525e7c9d7dc36c1cdd6af679eb9d
Naman Jain <naman1205jain@gmail.com>
2018-06-07 11:45:28 +0530

    added complete test

--- src/opts.py
+++ src/opts.py
@@ -42,6 +42,7 @@ class opts():
 		self.parser.add_argument('-regWeight', type = float, default = 0.2, help = 'depth regression loss weight')
 		self.parser.add_argument('-varWeight', type = float, default = 0, help = 'variance loss weight')
 
+		self.parser.add_argument('-completeTest', type = int, default = 0, help = 'weak label data ratio')
 		self.parser.add_argument('-startVal', type = int, default = 660, help = 'weak label data ratio')
 		self.parser.add_argument('-nVal', type = int, default = 120, help = 'weak label data ratio')
 

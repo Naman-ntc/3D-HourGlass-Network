b74839e53afff9e8ea68787e32a9b9851345b230
Naman Jain <naman1205jain@gmail.com>
2018-06-15 22:22:37 +0530

    scaled Features

--- src/model/scaledFeaturses.py
+++ src/model/scaledFeaturses.py
@@ -4,15 +4,22 @@ from .Layers3D import *
 
 class scaledFeaturesNet(object):
 	"""docstring for scaledFeaturesNet"""
-	def __init__(self, inChannels):
+	def __init__(self, inChannels, config):
 		super(scaledFeaturesNet, self).__init__()
 		self.inChannels = inChannels
 		self.reduceTemporal = nn.AvgPool3d((3,1,1),(2,1,1))
-		self.res1 = Residual3D(inChannels,inChannels)
-		self.res2 = Residual3D(inChannels,inChannels)
-		self.mp = nn.MaxPool3D()
-		self.layers = nn.Sequential(* self.layers)
-
+		self.layers = []
+		for c in config:
+			if c=='r':
+				self.layers.append(Residual(inChannels,inChannels))
+			if c=='rr':
+				self.layers.append(Residual(inChannels,inChannels//2))	
+			if c=='m':
+				self.layers.append(nn.MaxPool3d((1,2,2),(1,2,2)))
+			if c=='mt':
+				self.layers.append(nn.MaxPool3d(2,2))		
 	def forward(self, input):
-		out = self.layers(input)
+		out = input
+		out = self.reduceTemporal(out)
+		out = self.layers(out)
 		return out

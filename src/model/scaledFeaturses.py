4a2f6fbaa12b6b8d8815fcf23989805c1da140e2
Naman Jain <naman1205jain@gmail.com>
2018-06-16 13:26:29 +0530

    better scaled features

--- src/model/scaledFeaturses.py
+++ src/model/scaledFeaturses.py
@@ -10,14 +10,20 @@ class scaledFeaturesNet(object):
 		self.reduceTemporal = nn.AvgPool3d((3,1,1),(2,1,1))
 		self.layers = []
 		for c in config:
-			if c=='r':
-				self.layers.append(Residual(inChannels,inChannels))
-			if c=='rr':
-				self.layers.append(Residual(inChannels,inChannels//2))	
+			if c=='r3D':
+				self.layers.append(Residual3D(inChannels,inChannels))
+			if c=='rr3D':
+				self.layers.append(Residual3D(inChannels,inChannels//2))
+			if c=='r2D':
+				self.layers.append(Residual2D(inChannels,inChannels))
+			if c=='rr2D':
+				self.layers.append(Residual2D(inChannels,inChannels//2))		
 			if c=='m':
 				self.layers.append(nn.MaxPool3d((1,2,2),(1,2,2)))
 			if c=='mt':
 				self.layers.append(nn.MaxPool3d(2,2))		
+		self.layers = nn.Sequential(* self.layers)
+
 	def forward(self, input):
 		out = input
 		out = self.reduceTemporal(out)
